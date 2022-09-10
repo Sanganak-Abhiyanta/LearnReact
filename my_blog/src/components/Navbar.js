5e26c7968b0609dede39738667dfffcf0bf68f3d
NIITISH KUMAR <imsanganakabhiyanta@gmail.com>
2022-09-10 16:03:54 +0530

    I have created my_blog react application.
    In this Single page application I have implemented function as a props , css and props concept and useState concept and filter concept.

---
+++ my_blog/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>My Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/">New Blog</a>
+      </div>
+    </nav>
+  );
+}
