2c86cb69958d97aeb04da0f229cb1a0bcd46bc22
NIITISH KUMAR <imsanganakabhiyanta@gmail.com>
2022-09-11 22:07:32 +0530

    trying to implement custom Hooks
    In this commit I have learn and try to implement custom hooks.

--- my_blog/src/components/Home.js
+++ my_blog/src/components/Home.js
@@ -3,24 +3,42 @@ import BlogList from "./BlogList";
 
 export default function Home() {
   const [blogs, setBlogs] = useState(null);
-  const handleClick = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  const [isPending, setIsPending]=useState(true);
+  const [error, setError]=useState(null);
+
+  // const handleClick = (id) => {
+  //   const newBlogs = blogs.filter((blog) => blog.id !== id);
+  //   setBlogs(newBlogs);
+  // };
 
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
+    setTimeout(()=>{
+      fetch("http://localhost:8000/blogs")
       .then((res) => {
+        console.log(res);
+        if(!res.ok)
+        {
+          throw Error("Could not fetch the data for that resource.")
+        }
         return res.json();
       })
       .then((data) => {
         setBlogs(data);
-      });
+        setIsPending(false);
+        setError(null);
+      }).catch(err=>{
+        setIsPending(false);
+        setError(err.message);
+      })
+    },1000);
   }, []);
   return (
     <div className="home">
+      {error && <div>{error}</div> }
+      {isPending && <div>Loading....</div> }
       {blogs && (
-        <BlogList blogs={blogs} title="All Blogs!" handleClick={handleClick} />
+        // <BlogList blogs={blogs} title="All Blogs!" handleClick={handleClick} />
+        <BlogList blogs={blogs} title="All Blogs!"/>
       )}
       {/* <BlogList blogs={blogs.filter((blog)=>blog.author==="mario")} title="Mario Blogs!" handleClick={handleClick}/> */}
     </div>
---
+++ my_blog/src/components/useFetch.js
@@ -0,0 +1,1 @@
+// making custom hooks
