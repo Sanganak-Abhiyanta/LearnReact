48f32f7442df5360d96186c004caa45a03671e8f
NIITISH KUMAR <imsanganakabhiyanta@gmail.com>
2022-09-12 20:47:36 +0530

    Implemented custom hook in this commit.

--- my_blog/src/components/Home.js
+++ my_blog/src/components/Home.js
@@ -1,44 +1,51 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 export default function Home() {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending]=useState(true);
-  const [error, setError]=useState(null);
+  // code for use custom hooks
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
+  // const [blogs, setBlogs] = useState(null);
+  // const [isPending, setIsPending]=useState(true);
+  // const [error, setError]=useState(null);
 
   // const handleClick = (id) => {
   //   const newBlogs = blogs.filter((blog) => blog.id !== id);
   //   setBlogs(newBlogs);
   // };
 
-  useEffect(() => {
-    setTimeout(()=>{
-      fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        console.log(res);
-        if(!res.ok)
-        {
-          throw Error("Could not fetch the data for that resource.")
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-        setIsPending(false);
-        setError(null);
-      }).catch(err=>{
-        setIsPending(false);
-        setError(err.message);
-      })
-    },1000);
-  }, []);
+  // useEffect(() => {
+  //   setTimeout(()=>{
+  //     fetch("http://localhost:8000/blogs")
+  //     .then((res) => {
+  //       console.log(res);
+  //       if(!res.ok)
+  //       {
+  //         throw Error("Could not fetch the data for that resource.")
+  //       }
+  //       return res.json();
+  //     })
+  //     .then((data) => {
+  //       setBlogs(data);
+  //       setIsPending(false);
+  //       setError(null);
+  //     }).catch(err=>{
+  //       setIsPending(false);
+  //       setError(err.message);
+  //     })
+  //   },1000);
+  // }, []);
   return (
     <div className="home">
-      {error && <div>{error}</div> }
-      {isPending && <div>Loading....</div> }
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading....</div>}
       {blogs && (
         // <BlogList blogs={blogs} title="All Blogs!" handleClick={handleClick} />
-        <BlogList blogs={blogs} title="All Blogs!"/>
+        <BlogList blogs={blogs} title="All Blogs!" />
       )}
       {/* <BlogList blogs={blogs.filter((blog)=>blog.author==="mario")} title="Mario Blogs!" handleClick={handleClick}/> */}
     </div>
--- my_blog/src/components/useFetch.js
+++ my_blog/src/components/useFetch.js
@@ -1,1 +1,33 @@
 // making custom hooks
+// import React from "react";
+import { useState, useEffect } from "react";
+
+export default function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(url)
+        .then((res) => {
+        //   console.log(res);
+          if (!res.ok) {
+            throw Error("Could not fetch the data for that resource.");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
+        });
+    }, 1000);
+  }, [url]);
+
+  return {data,isPending,error};
+}
