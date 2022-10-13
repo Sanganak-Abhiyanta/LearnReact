642cfecdfe63d27da54fcb7fc43cab5d73c8ca77
NIITISH KUMAR <imsanganakabhiyanta@gmail.com>
2022-10-13 13:26:40 +0530

    Removing the local state.
    Still incomplete counter application.

--- counter-app/src/components/Counters.jsx
+++ counter-app/src/components/Counters.jsx
@@ -2,22 +2,42 @@ import React from "react";
 import Counter from "./counter";
 import { useState } from "react";
 export default function Counters() {
-  const [counter, setCounter] = useState([
+  const [counters, setCounter] = useState([
     { id: 1, value: 4 },
     { id: 2, value: 3 },
     { id: 3, value: 4 },
     { id: 4, value: 5 },
     { id: 5, value: 6 },
   ]);
+  const handleIncrement = counter => {
+    const count=[...counters];
+    const index=count.indexOf(counter);
+    count[index]={...counter};
+    count[index].value++;
+    setCounter(count);
+    console.log(count[0]);
+  };
+  const handleDecrement = (counter) => {
+  };
+  const handleReset = (counter) => {
+
+  };
   const handleDelte = (counterId) => {
-    const counte = counter.filter((m) => m.id !== counterId);
+    const counte = counters.filter((m) => m.id !== counterId);
     setCounter(counte);
     console.log("Deleted " + counterId);
   };
   return (
     <>
-      {counter.map((m) => (
-        <Counter key={m.id} counter={m}  onDelete={handleDelte}>
+      {counters.map((m) => (
+        <Counter
+          key={m.id}
+          counter={m}
+          onDelete={handleDelte}
+          handleIncrement={handleIncrement}
+          handleDecrement={handleDecrement}
+          handleReset={handleReset}
+        >
           <h2>CounterApp #{m.id}</h2>
         </Counter>
       ))}
--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,18 +1,6 @@
 import React, { Component } from "react";
 
 export default class Counter extends Component {
-  state = {
-    count: this.props.counter.value,
-  };
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-  handleDecrement = () => {
-    this.setState({ count: this.state.count - 1 });
-  };
-  handleReset = () => {
-    this.setState({ count: 0 });
-  };
   // =========================>Bining Event Handlers<===============================================
 
   render() {
@@ -22,18 +10,33 @@ export default class Counter extends Component {
         {this.props.children}
         <span className={this.getButtonClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => {
+            this.props.handleIncrement(this.props.counter);
+          }}
           className="btn btn-secondary mx-2"
         >
           Increment
         </button>
-        <button onClick={this.handleDecrement} className="btn btn-success mx-2">
+        <button
+          onClick={() => {
+            this.props.handleDecrement(this.props.counter);
+          }}
+          className="btn btn-success mx-2"
+        >
           Decrement
         </button>
-        <button onClick={this.handleReset} className="btn btn-warning mx-2">
+        <button
+          onClick={() => {
+            this.props.handleReset(this.props.counter);
+          }}
+          className="btn btn-warning mx-2"
+        >
           Reset
         </button>
-        <button onClick={()=>this.props.onDelete(this.props.counter.id)} className="btn btn-danger m-2">
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger m-2"
+        >
           Delete
         </button>
       </div>
@@ -42,12 +45,12 @@ export default class Counter extends Component {
   // ==============================================
   getButtonClasses() {
     let classes = "btn m-2 mx-2 my-2 m-2 btn-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
   // =========================================================
   formatCount() {
-    const { count } = this.state;
+    const { count } = this.props.counter;
     return count === 0 ? "Zero" : count;
   }
 }
