8515d4c62ea47806bd672cf9e5fbe8d1e744381d
NIITISH KUMAR <imsanganakabhiyanta@gmail.com>
2022-10-21 22:48:07 +0530

    In this commit i Have use and learn about object destructuring ..

--- counter-app/src/components/Counters.jsx
+++ counter-app/src/components/Counters.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import Counter from "./counter";
-
-export default function Counters(props) {
+//when we remove "props" and "props." and use direct name of props in the child component it is called object destructuring , we have to use {}
+ 
+export default function Counters({resetAll,counter,onDelete,handleDecrement,handleIncrement,handleReset}) {
   
   return (
     <>
-      <button onClick={props.resetAll} className="btn btn-warning mx-2 btn-sm">
+      <button onClick={resetAll} className="btn btn-warning mx-2 btn-sm">
         Reset
       </button>
-      {props.counter.map((m) => (
+      {counter.map((m) => (
         <Counter
           key={m.id}
           counter={m}
-          onDelete={props.onDelete}
-          handleIncrement={props.handleIncrement}
-          handleDecrement={props.handleDecrement}
-          handleReset={props.handleReset}
+          onDelete={onDelete}
+          handleIncrement={handleIncrement}
+          handleDecrement={handleDecrement}
+          handleReset={handleReset}
         >
           <h2>CounterApp #{m.id}</h2>
         </Counter>
--- counter-app/src/components/NavBar.jsx
+++ counter-app/src/components/NavBar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 //stateless functional components
-const NavBar = (props) => {
+const NavBar = ({totalCounters}) => {
   return (
     <>
       <nav className="navbar navbar-light bg-light">
         <h1 className="navbar-brand">
           Navbar{" "}
           <span className="badge badge-pill badge-info bg-dark">
-            {props.totalCounters}
+            {totalCounters}
           </span>
         </h1>
       </nav>
