e38c37bc04ea7af07f0fd749209f1411cf6a8844
NIITISH KUMAR <imsanganakabhiyanta@gmail.com>
2022-10-28 23:50:36 +0530

    Move all the code of raise sort method to another component.
    we can use this to another table  also.

--- vidly/src/components/MoviesTable.jsx
+++ vidly/src/components/MoviesTable.jsx
@@ -1,32 +1,23 @@
 import Like from "./common/Like";
 import React, { Component } from "react";
+import TableHeaderSort from "./common/TableHeaderSort";
 //at first we use functional component and then we use class component because we have to
 //
 export default class MoviesTable extends Component {
-  raiseSort = (path) => {
-    const sortColumn = { ...this.props.sortColumn };
-    if (sortColumn.path === path)
-      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-    else {
-      sortColumn.path = path;
-      sortColumn.order = "asc";
-    }
-    this.props.onSort(sortColumn);
-  };
+  columns=[
+    {path:'title',label:"Title"}, 
+    {path:'genre.name',label:"Genre"},
+    {path:'numberInStock',label:"Stock"},
+    {path:'dailyRentalRate',label:"Rate"},
+    {},//for Like
+    {}//for Delete
+
+];
   render() {
-    const { movies, onDelete, onLike} = this.props;
+    const { movies, onDelete, onLike,sortColumn,onSort } = this.props;
     return (
       <table className="table">
-        <thead>
-          <tr>
-            <th onClick={() => this.raiseSort("title")}>Title</th>
-            <th onClick={() => this.raiseSort("genre.name")}>Genre</th>
-            <th onClick={() => this.raiseSort("numberInStock")}>Stock</th>
-            <th onClick={() => this.raiseSort("dailyRentalRate")}>Rate</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
+        <TableHeaderSort columns={this.columns} sortColumn={sortColumn} onSort={onSort} />
         <tbody>
           {movies.map((movie) => {
             return (
