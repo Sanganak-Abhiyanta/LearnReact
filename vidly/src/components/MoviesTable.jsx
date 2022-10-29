352e7cc3c941f6355b583db64f6d29b7b83960f1
NIITISH KUMAR <imsanganakabhiyanta@gmail.com>
2022-10-29 12:25:10 +0530

    extracting done for moviesTable.
    move all the codes of table to tableBody components.

--- vidly/src/components/MoviesTable.jsx
+++ vidly/src/components/MoviesTable.jsx
@@ -1,46 +1,43 @@
 import Like from "./common/Like";
 import React, { Component } from "react";
 import TableHeaderSort from "./common/TableHeaderSort";
+import TableBody from "./common/TableBody";
 //at first we use functional component and then we use class component because we have to
 //
 export default class MoviesTable extends Component {
-  columns=[
-    {path:'title',label:"Title"}, 
-    {path:'genre.name',label:"Genre"},
-    {path:'numberInStock',label:"Stock"},
-    {path:'dailyRentalRate',label:"Rate"},
-    {},//for Like
-    {}//for Delete
-
-];
+  columns = [
+    { path: "title", label: "Title" },
+    { path: "genre.name", label: "Genre" },
+    { path: "numberInStock", label: "Stock" },
+    { path: "dailyRentalRate", label: "Rate" },
+    {
+      key: "like",
+      content:movie=> (<Like onClick={() => this.props.onLike(movie)} liked={movie.liked}></Like>)
+    }, //for Like
+    {
+      key: "delete",
+      content:movie=> (
+        <button
+          onClick={() => this.props.onDelete(movie)}
+          className="btn btn-danger btn-sm"
+        >
+          Delete
+        </button>
+      ),
+    }, //for Delete
+  ];
   render() {
-    const { movies, onDelete, onLike,sortColumn,onSort } = this.props;
+    // const { movies, onDelete, onLike, sortColumn, onSort } = this.props;
+    const { movies, sortColumn, onSort } = this.props;
     return (
       <table className="table">
-        <TableHeaderSort columns={this.columns} sortColumn={sortColumn} onSort={onSort} />
-        <tbody>
-          {movies.map((movie) => {
-            return (
-              <tr key={movie._id}>
-                <td>{movie.title}</td>
-                <td>{movie.genre.name}</td>
-                <td>{movie.numberInStock}</td>
-                <td>{movie.dailyRentalRate}</td>
-                <td>
-                  <Like onClick={() => onLike(movie)} liked={movie.liked} />
-                </td>
-                <td>
-                  <button
-                    onClick={() => onDelete(movie)}
-                    className="btn btn-danger btn-sm"
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
+        <TableHeaderSort
+          columns={this.columns}
+          sortColumn={sortColumn}
+          onSort={onSort}
+        />
+        <TableBody data={movies} columns={this.columns} />
+       
       </table>
     );
   }
--- vidly/src/components/common/TableHeaderSort.jsx
+++ vidly/src/components/common/TableHeaderSort.jsx
@@ -19,7 +19,7 @@ export default class TableHeaderSort extends Component {
       <thead>
         <tr>
           {this.props.columns.map(column => 
-            (<th key={column.path} onClick={() => this.raiseSort(column.path)}>{column.label}</th>)
+            (<th key={column.path || column.key} onClick={() => this.raiseSort(column.path)}>{column.label}</th>)
           )}
         </tr>
       </thead>
