1db83a090e5e616e180f3b73af054ab124c61c6a
NIITISH KUMAR <imsanganakabhiyanta@gmail.com>
2022-10-29 14:57:14 +0530

    method is encapsulate , and a new method created name getPageData.

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -45,13 +45,10 @@ export default class Movies extends Component {
 
   // =====================> handleSort method <========================
   handleSort = (sortColumn) => {
-   
     this.setState({ sortColumn });
   };
-
-  render() {
-    const { length: count } = this.state.movies;
-    // ==========================>we can use object destructuring <============================
+  //=============> getPageData <=========================
+  getPageData = () => {
     const {
       currentPage,
       selectedGenre,
@@ -59,9 +56,6 @@ export default class Movies extends Component {
       movies: allMovies,
       sortColumn,
     } = this.state;
-
-    if (count === 0) return <h1>There are no movies in the Database</h1>;
-    // now we need to filter movies according to genre
     const filtered =
       selectedGenre && selectedGenre._id
         ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
@@ -69,6 +63,18 @@ export default class Movies extends Component {
     const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
 
     const movies = Paginate(sorted, currentPage, pageSize);
+
+    return { totalCount: filtered.length, data: movies };
+  };
+
+  render() {
+    const { length: count } = this.state.movies;
+    const { currentPage, pageSize, sortColumn } = this.state;
+    // ==========================>we can use object destructuring <============================
+
+    if (count === 0) return <h1>There are no movies in the Database</h1>;
+    // now we need to filter movies according to genre
+    const { totalCount, data: movies } = this.getPageData();
     return (
       <div className="m-5">
         <div className="row">
@@ -81,7 +87,7 @@ export default class Movies extends Component {
           </div>
           <div className="col">
             {" "}
-            <p>Showing {filtered.length} movies in the database.</p>
+            <p>Showing {totalCount} movies in the database.</p>
             <MoviesTable
               movies={movies}
               sortColumn={sortColumn}
@@ -91,7 +97,7 @@ export default class Movies extends Component {
             />
             <Pagination
               // itemsCount={count}
-              itemsCount={filtered.length}
+              itemsCount={totalCount}
               currentPage={currentPage}
               pageSize={pageSize}
               onPageChange={this.handlePageChange}
