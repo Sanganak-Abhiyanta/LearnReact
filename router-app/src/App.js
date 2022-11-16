7123e9809276d8df4c499a58463f0998b82bc5e3
NIITISH KUMAR <imsanganakabhiyanta@gmail.com>
2022-11-16 20:14:21 +0530

    Have learned how to use Switch in react-router-dom in version 4.3.1

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import NavBar from "./components/NavBar";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Products from "./components/Products";
 import Posts from "./components/Posts";
 import Dashboard from "./components/admin/Dashboard";
 import ProductDetails from "./components/productDetails";
-import Home from "./components/Home"
-
+import Home from "./components/Home";
 
 export default class App extends Component {
   render() {
@@ -14,14 +13,16 @@ export default class App extends Component {
       <Router>
         <div>
           <NavBar />
-          <div className="container">
-            <Routes>
-              <Route path="/products/:id" render={()=> <ProductDetails id={id} />}/>
-              <Route path="/products" element={<Products sortBy="newest" />}/>
-              <Route path="/posts" element={<Posts/>}/>
-              <Route path="/admin" element={<Dashboard/>}/>
-              <Route path="/"  element={<Home/>}/>
-            </Routes>
+          <div className="content">
+           <Switch>
+           <Route path="/products" component={Products} />
+           <Route path="/posts" component={Posts} />
+           <Route path="/admin" component={Dashboard} />
+           <Route path="/" component={Home} />
+           {/* <Route path="/" exact component={Home} /> //exact is one solution and Switch is another one */}
+           
+           </Switch>
+           
           </div>
         </div>
       </Router>
